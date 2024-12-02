92b8461abf92bfcab8d775dd9dff6fb58cac881e
bhanuprakash <bhanuprakashande9@gmail.com>
2024-12-02 14:11:55 +0530

    React Vendor DashBoard

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import LandingPage from './vendorDashboard/pages/LandingPage'
+import "./App.css"
+import { Routes,Route } from 'react-router-dom'
+import NotFound from './vendorDashboard/compnents/NotFound'
+
+
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/*' element={<NotFound/>}/>
+      </Routes>
+        
+    </div>
+  )
+}
+
+export default App
